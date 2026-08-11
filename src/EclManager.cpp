f221ea245b589716b23b35ccf0eccd0974b1a68f
Fuxuan-CN <3072252442@qq.com>
2026-08-11 17:57:14 +0800

    RunEcl 新增 op147 写全局 0x4ea290

--- src/EclManager.cpp
+++ src/EclManager.cpp
@@ -565,6 +565,9 @@ restart:
             case 150: // opcode 151 = 设置 unk3324 bit26
                 enemy->unk3324 = (enemy->unk3324 & ~0x4000000) | ((instr->args[0].b[0] & 1) << 0x1a);
                 goto skipInstr;
+            case 146: // opcode 147 = 写全局 0x4ea290
+                *(volatile u32 *)0x4ea290 = ECL_IVAL(0);
+                goto skipInstr;
             case 144: // opcode 145 = 设置 unk3324 bit25
                 enemy->unk3324 = (enemy->unk3324 & ~0x2000000) | ((instr->args[0].b[0] & 1) << 0x19);
                 goto skipInstr;
